5beb1d3c3703f3cac4f6fbc5e8dab5f1e6838cc9
strixsoul <rsilveira136@gmail.com>
2021-07-18 19:12:54 -0300

    feat: 🎸 update restaurant controller and route created

--- src/shared/infra/http/routes/restaurants.routes.ts
+++ src/shared/infra/http/routes/restaurants.routes.ts
@@ -1,14 +1,15 @@
+import { CreateRestaurantController } from '@modules/restaurants/useCases/createRestaurant/CreateRestaurantController';
 import { ListAllRestaurantsController } from '@modules/restaurants/useCases/listAllRestaurants/ListAllRestaurantsController';
+import { ListOneRestaurantController } from '@modules/restaurants/useCases/listOneRestaurant/ListOneRestaurantController';
+import { UpdateRestaurantController } from '@modules/restaurants/useCases/updateRestaurant/UpdateRestaurantController';
 import { Router } from 'express';
-
-import { CreateRestaurantController } from '../../../../modules/restaurants/useCases/createRestaurant/CreateRestaurantController';
-import { ListOneRestaurantController } from '../../../../modules/restaurants/useCases/listOneRestaurant/ListOneRestaurantController';
 
 const restaurantsRoutes = Router();
 
 const listAllRestaurantController = new ListAllRestaurantsController();
 const listOneRestaurantController = new ListOneRestaurantController();
 const createRestaurantController = new CreateRestaurantController();
+const updateRestaurantController = new UpdateRestaurantController();
 
 restaurantsRoutes.get('/', listAllRestaurantController.handle);
 
@@ -16,9 +17,7 @@ restaurantsRoutes.get('/:id', listOneRestaurantController.handle);
 
 restaurantsRoutes.post('/', createRestaurantController.handle);
 
-restaurantsRoutes.put('/:id', (request, response) => {
-  response.status(201).json('oi');
-});
+restaurantsRoutes.put('/:id', updateRestaurantController.handle);
 
 restaurantsRoutes.delete('/:id', (request, response) => {
   response.status(201).json('oi');
