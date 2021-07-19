12f57aba447e8c5d27c7d03b01d6688269d4bf46
strixsoul <rsilveira136@gmail.com>
2021-07-19 15:13:44 -0300

    feat: 🎸 created postgres reposity and sql to create product

---
+++ src/modules/products/repositories/implementations/CategoriesPostgresRepository.ts
@@ -0,0 +1,38 @@
+import { Category } from '@modules/products/infra/postgres/entities/Category';
+import { createConnection } from 'database/connection';
+
+import { ICategoriesRepository } from '../ICategoriesRepository';
+
+class CategoriesPostgresRepository implements ICategoriesRepository {
+  async create({
+    id,
+    name,
+    restaurantId,
+  }: {
+    id: string;
+    name: string;
+    restaurantId: string;
+  }): Promise<Category> {
+    const client = await createConnection();
+
+    const { rows } = await client.query(
+      `INSERT INTO CATEGORIES(ID, NAME, RESTAURANT_ID) VALUES($1, $2, $3) RETURNING ID, NAME`,
+      [id, name, restaurantId],
+    );
+
+    return rows[0];
+  }
+
+  async findByName(name: string): Promise<Category> {
+    const client = await createConnection();
+
+    const { rows } = await client.query(
+      `SElECT ID, NAME FROM CATEGORIES WHERE NAME = $1`,
+      [name],
+    );
+
+    return rows[0];
+  }
+}
+
+export { CategoriesPostgresRepository };
