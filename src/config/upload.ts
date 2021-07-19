44fc9b433edc05dac051788125c2da12c1675978
strixsoul <rsilveira136@gmail.com>
2021-07-19 00:14:29 -0300

    feat: 🎸 created storage provider and upload photo config

---
+++ src/config/upload.ts
@@ -0,0 +1,39 @@
+import crypto from 'crypto';
+import multer, { StorageEngine } from 'multer';
+import path from 'path';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+interface IUpload {
+  driver: 's3' | 'local';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+}
+
+export const upload = {
+  driver: process.env.STORAGE_DRIVER || 'local',
+
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(req, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+  },
+} as IUpload;
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,21 @@
+import { upload } from '@config/upload';
+import { OpeningHoursPostgresRepository } from '@modules/restaurants/repositories/implementations/OpeningHoursPostgresRepository';
+import { RestaurantsPostgresRepository } from '@modules/restaurants/repositories/implementations/RestaurantsPostgresRepositories';
+import { IOpeningHoursRepository } from '@modules/restaurants/repositories/IOpeningHoursRepository';
+import { IRestaurantsRepository } from '@modules/restaurants/repositories/IRestaurantRepository';
 import { container } from 'tsyringe';
 
-import { OpeningHoursPostgresRepository } from '../../modules/restaurants/repositories/implementations/OpeningHoursPostgresRepository';
-import { RestaurantsPostgresRepository } from '../../modules/restaurants/repositories/implementations/RestaurantsPostgresRepositories';
-import { IOpeningHoursRepository } from '../../modules/restaurants/repositories/IOpeningHoursRepository';
-import { IRestaurantsRepository } from '../../modules/restaurants/repositories/IRestaurantRepository';
+import { LocalStorageProvider } from './providers/StorageProvider/implementations/LocalStorageProvider';
+import { IStorageProvider } from './providers/StorageProvider/IStorageProvider';
+
+const storageProviders = {
+  local: LocalStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  storageProviders[upload.driver],
+);
 
 container.registerSingleton<IRestaurantsRepository>(
   'RestaurantsRepository',
