82ffd6a869cbd83270eef1f519a973119e6ce3bb
strixsoul <rsilveira136@gmail.com>
2021-07-20 03:44:23 -0300

    feat: 🎸 upload photo product controller and route created

--- src/shared/infra/http/routes/products.routes.ts
+++ src/shared/infra/http/routes/products.routes.ts
@@ -1,10 +1,15 @@
+import { upload } from '@config/upload';
 import { CreateRestaurantProductController } from '@modules/products/useCases/createRestaurantProduct/CreateRestaurantProductController';
 import { DeleteRestaurantProductController } from '@modules/products/useCases/deleteRestaurantProduct/DeleteRestaurantProductController';
 import { ListAllRestaurantProductsController } from '@modules/products/useCases/listAllRestaurantProducts/ListAllRestaurantProductsController';
 import { UpdateRestaurantProductController } from '@modules/products/useCases/updateRestaurantProduct/UpdateRestaurantProductController';
+import { UploadPhotoRestaurantProductController } from '@modules/products/useCases/uploadPhotoRestaurantProduct/UploadPhotoRestaurantProductController';
 import { Router } from 'express';
+import multer from 'multer';
 
 const productsRoutes = Router();
+
+const uploadPhoto = multer(upload.multer);
 
 const listAllRestaurantProductsController =
   new ListAllRestaurantProductsController();
@@ -14,6 +19,8 @@ const updateRestaurantProductController =
   new UpdateRestaurantProductController();
 const deleteRestaurantProductController =
   new DeleteRestaurantProductController();
+const uploadPhotoRestaurantProductController =
+  new UploadPhotoRestaurantProductController();
 
 productsRoutes.get(
   '/:restaurantId',
@@ -30,6 +37,12 @@ productsRoutes.put(
 productsRoutes.delete(
   '/:restaurantId/:id',
   deleteRestaurantProductController.handle,
+);
+
+productsRoutes.patch(
+  '/photo/:restaurantId/:id',
+  uploadPhoto.single('photo'),
+  uploadPhotoRestaurantProductController.handle,
 );
 
 export { productsRoutes };
