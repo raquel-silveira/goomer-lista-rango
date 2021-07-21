5d3a9e0989763caa1906203470d176426ebc33d8
strixsoul <rsilveira136@gmail.com>
2021-07-21 17:41:41 -0300

    test: 💍 updated tests to validate hours and dates

--- src/modules/products/useCases/updateRestaurantProduct/UpdateRestaurantProductUseCase.spec.ts
+++ src/modules/products/useCases/updateRestaurantProduct/UpdateRestaurantProductUseCase.spec.ts
@@ -72,6 +72,46 @@ describe('Update Restaurant Product', () => {
     expect(productUpdated.name).toBe('Biscoito');
   });
 
+  it('should be able to update a product with promotion', async () => {
+    const restaurant = await restaurantsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460541',
+      name: 'Restaurante Goomer',
+      address: 'Rua São João',
+      number: '500',
+      neighborhood: 'Jardim Tóquio',
+      city: 'Sorocaba',
+      state: 'SP',
+      country: 'Brasil',
+      postal_code: '18279-050',
+    });
+
+    const product = await productsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460542',
+      name: 'Bolacha',
+      price: 3.5,
+      category_id: '1',
+      restaurantId: restaurant.id,
+    });
+
+    const productUpdated = await updateRestaurantProductUseCase.execute({
+      id: product.id,
+      name: 'Biscoito',
+      price: 3.5,
+      category: 'doce',
+      restaurantId: restaurant.id,
+      promotion: {
+        description: 'metade do preço',
+        start_date: '2021-05-05',
+        finish_date: '2021-05-06',
+        start_time: '10:00',
+        finish_time: '10:00',
+        price_promotion: 2.5,
+      },
+    });
+
+    expect(productUpdated.name).toBe('Biscoito');
+  });
+
   it('should be able to update a product without category', async () => {
     const restaurant = await restaurantsRepositoryInMemory.create({
       id: 'a29fc323-5365-4baf-8461-fe2cae460541',
@@ -166,6 +206,252 @@ describe('Update Restaurant Product', () => {
         restaurantId: restaurant.id,
       }),
     ).rejects.toEqual(new AppError('Name is required'));
+  });
+
+  it('should not be able to update if promotion dows not have all date and time fields', async () => {
+    const restaurant = await restaurantsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460541',
+      name: 'Restaurante Goomer',
+      address: 'Rua São João',
+      number: '500',
+      neighborhood: 'Jardim Tóquio',
+      city: 'Sorocaba',
+      state: 'SP',
+      country: 'Brasil',
+      postal_code: '18279-050',
+    });
+
+    const product = await productsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460542',
+      name: 'Bolacha',
+      price: 3.5,
+      category_id: '1',
+      restaurantId: restaurant.id,
+    });
+
+    await expect(
+      updateRestaurantProductUseCase.execute({
+        id: product.id,
+        name: 'Biscoito',
+        price: 3.5,
+        category: 'doce',
+        restaurantId: restaurant.id,
+        promotion: {
+          description: 'metade do preço',
+          start_date: null,
+          finish_date: '2021-05-06',
+          start_time: '10:00',
+          finish_time: '10:00',
+          price_promotion: 2.5,
+        },
+      }),
+    ).rejects.toEqual(
+      new AppError('Promotion must have all date and time fields'),
+    );
+  });
+
+  it('should not be able to update if promotion date format is invalid', async () => {
+    const restaurant = await restaurantsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460541',
+      name: 'Restaurante Goomer',
+      address: 'Rua São João',
+      number: '500',
+      neighborhood: 'Jardim Tóquio',
+      city: 'Sorocaba',
+      state: 'SP',
+      country: 'Brasil',
+      postal_code: '18279-050',
+    });
+
+    const product = await productsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460542',
+      name: 'Bolacha',
+      price: 3.5,
+      category_id: '1',
+      restaurantId: restaurant.id,
+    });
+
+    await expect(
+      updateRestaurantProductUseCase.execute({
+        id: product.id,
+        name: 'Biscoito',
+        price: 3.5,
+        category: 'doce',
+        restaurantId: restaurant.id,
+        promotion: {
+          description: 'metade do preço',
+          start_date: '10/10/2021',
+          finish_date: '11/10/2021',
+          start_time: '10:00',
+          finish_time: '10:00',
+          price_promotion: 2.5,
+        },
+      }),
+    ).rejects.toEqual(new AppError('Date format invalid'));
+  });
+
+  it('should not be able to update if promotion hour format is invalid', async () => {
+    const restaurant = await restaurantsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460541',
+      name: 'Restaurante Goomer',
+      address: 'Rua São João',
+      number: '500',
+      neighborhood: 'Jardim Tóquio',
+      city: 'Sorocaba',
+      state: 'SP',
+      country: 'Brasil',
+      postal_code: '18279-050',
+    });
+
+    const product = await productsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460542',
+      name: 'Bolacha',
+      price: 3.5,
+      category_id: '1',
+      restaurantId: restaurant.id,
+    });
+
+    await expect(
+      updateRestaurantProductUseCase.execute({
+        id: product.id,
+        name: 'Biscoito',
+        price: 3.5,
+        category: 'doce',
+        restaurantId: restaurant.id,
+        promotion: {
+          description: 'metade do preço',
+          start_date: '2021-10-10',
+          finish_date: '2021-10-11',
+          start_time: '10:00',
+          finish_time: '25:00',
+          price_promotion: 2.5,
+        },
+      }),
+    ).rejects.toEqual(new AppError('Hour format invalid'));
+  });
+
+  it('should not be able to update with promotion without price', async () => {
+    const restaurant = await restaurantsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460541',
+      name: 'Restaurante Goomer',
+      address: 'Rua São João',
+      number: '500',
+      neighborhood: 'Jardim Tóquio',
+      city: 'Sorocaba',
+      state: 'SP',
+      country: 'Brasil',
+      postal_code: '18279-050',
+    });
+
+    const product = await productsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460542',
+      name: 'Bolacha',
+      price: 3.5,
+      category_id: '1',
+      restaurantId: restaurant.id,
+    });
+
+    await expect(
+      updateRestaurantProductUseCase.execute({
+        id: product.id,
+        name: 'Biscoito',
+        price: 3.5,
+        category: 'doce',
+        restaurantId: restaurant.id,
+        promotion: {
+          description: 'metade do preço',
+          start_date: '2021-10-10',
+          finish_date: '2021-10-11',
+          start_time: '10:00',
+          finish_time: '15:00',
+          price_promotion: null,
+        },
+      }),
+    ).rejects.toEqual(new AppError('Price is required to promotion'));
+  });
+
+  it('should not be able to update if promotion start date if greater then finish date', async () => {
+    const restaurant = await restaurantsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460541',
+      name: 'Restaurante Goomer',
+      address: 'Rua São João',
+      number: '500',
+      neighborhood: 'Jardim Tóquio',
+      city: 'Sorocaba',
+      state: 'SP',
+      country: 'Brasil',
+      postal_code: '18279-050',
+    });
+
+    const product = await productsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460542',
+      name: 'Bolacha',
+      price: 3.5,
+      category_id: '1',
+      restaurantId: restaurant.id,
+    });
+
+    await expect(
+      updateRestaurantProductUseCase.execute({
+        id: product.id,
+        name: 'Biscoito',
+        price: 3.5,
+        category: 'doce',
+        restaurantId: restaurant.id,
+        promotion: {
+          description: 'metade do preço',
+          start_date: '2021-10-15',
+          finish_date: '2021-10-11',
+          start_time: '10:00',
+          finish_time: '15:00',
+          price_promotion: 2.35,
+        },
+      }),
+    ).rejects.toEqual(
+      new AppError('Start date does not can be greater then finish date'),
+    );
+  });
+
+  it('should not be able to update if promotion times does not have a minimum interval of 15 minutes', async () => {
+    const restaurant = await restaurantsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460541',
+      name: 'Restaurante Goomer',
+      address: 'Rua São João',
+      number: '500',
+      neighborhood: 'Jardim Tóquio',
+      city: 'Sorocaba',
+      state: 'SP',
+      country: 'Brasil',
+      postal_code: '18279-050',
+    });
+
+    const product = await productsRepositoryInMemory.create({
+      id: 'a29fc323-5365-4baf-8461-fe2cae460542',
+      name: 'Bolacha',
+      price: 3.5,
+      category_id: '1',
+      restaurantId: restaurant.id,
+    });
+
+    await expect(
+      updateRestaurantProductUseCase.execute({
+        id: product.id,
+        name: 'Biscoito',
+        price: 3.5,
+        category: 'doce',
+        restaurantId: restaurant.id,
+        promotion: {
+          description: 'metade do preço',
+          start_date: '2021-10-10',
+          finish_date: '2021-10-10',
+          start_time: '10:00',
+          finish_time: '10:14',
+          price_promotion: 2.35,
+        },
+      }),
+    ).rejects.toEqual(
+      new AppError('Times must have a minimum interval of 15 minutes'),
+    );
   });
 
   it('should not be able to update without restaurant id param', async () => {
