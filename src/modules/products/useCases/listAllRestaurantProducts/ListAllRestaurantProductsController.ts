0c000e721e3b977de503b7ae19027a4408dfac32
strixsoul <rsilveira136@gmail.com>
2021-07-21 18:31:16 -0300

    fix: 🐛 fixed name file

---
+++ src/modules/products/useCases/listAllRestaurantProducts/ListAllRestaurantProductsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListAllRestaurantProductsUseCase } from './ListAllRestaurantProductsUseCase';
+
+class ListAllRestaurantProductsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { restaurantId } = request.params;
+
+    const listAllRestaurantProductsUseCase = container.resolve(
+      ListAllRestaurantProductsUseCase,
+    );
+
+    const products = await listAllRestaurantProductsUseCase.execute({
+      restaurantId,
+    });
+
+    return response.json(products);
+  }
+}
+
+export { ListAllRestaurantProductsController };
